c18856c0195c5bec4513e8df062068cba8fd2591
Tilman <tilman@medallyon.me>
2022-08-06 17:07:47 +0100

    Update the sample 📜

--- Samples/PlayerStats/Scripts/PlayerStats.cs
+++ Samples/PlayerStats/Scripts/PlayerStats.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 namespace Medallyon
 {
+    // A simple example of saving & restoring a Guid
     public class PlayerStats : MonoBehaviour, ISaveable
     {
-        [Save] private Guid _playerID = Guid.Empty;
-        public string ID;
+        [Save] private Guid _playerID = Guid.NewGuid();
+
+        // OnFirstLoad is called the first ever time the game is started.
+        public void OnFirstLoad()
+        {
+            // This is for illustration purposes only.
+            _playerID = Guid.NewGuid();
+        }
+
+        // OnRestore is called when variables for this component are loaded and passed through.
+        public void OnRestore(Dictionary<string, object> data)
+        {
+            _playerID = Guid.Parse((string)data[nameof(_playerID)]);
+        }
 
         private void Start()
         {
-            Debug.Log(_playerID);
-            ID = _playerID.ToString();
-        }
-
-        public void OnRestore(bool isFirstLoad, Dictionary<string, object> data)
-        {
-            Debug.Log($"isFirstLoad: {isFirstLoad}");
-            _playerID = isFirstLoad ? Guid.NewGuid() : Guid.Parse((string)data[nameof(_playerID)]);
+            Debug.Log($"_playerID = {_playerID}");
         }
     }
 }
