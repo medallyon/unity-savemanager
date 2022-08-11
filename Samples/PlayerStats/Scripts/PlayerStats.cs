6b88f534ef294526a7c4e2ba77b502b5bb1637b4
Tilman <tilman@medallyon.me>
2022-08-11 21:38:23 +0100

    Update the sample 📜
    a

--- Samples/PlayerStats/Scripts/PlayerStats.cs
+++ Samples/PlayerStats/Scripts/PlayerStats.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,24 +6,33 @@ namespace Medallyon
     // A simple example of saving & restoring a Guid
     public class PlayerStats : MonoBehaviour, ISaveable
     {
-        [Save] private Guid _playerID = Guid.NewGuid();
+        [Save] public int PlayerId;
+        [Save] public string PlayerName;
+        [Save] private double Experience { get; set; }
+
+        private void Start()
+        {
+            Debug.Log($"PlayerId = {PlayerId}, PlayerName = {PlayerName}, Experience = {Experience}");
+        }
 
         // OnFirstLoad is called the first ever time the game is started.
         public void OnFirstLoad()
         {
-            // This is for illustration purposes only.
-            _playerID = Guid.NewGuid();
+            // Initialize any variables for the very first time here or simply as part of your constructor.
+            PlayerId = 42;
+            PlayerName = "Rufus";
+            Experience = Random.Range(0f, 1000f);
         }
 
         // OnRestore is called when variables for this component are loaded and passed through.
         public void OnRestore(Dictionary<string, object> data)
         {
-            _playerID = Guid.Parse((string)data[nameof(_playerID)]);
-        }
+            // Remember: 'data' is a Dictionary mapping the variable name to the last saved value of that variable.
 
-        private void Start()
-        {
-            Debug.Log($"_playerID = {_playerID}");
+            // Restoring primitives like this is usually not needed - but it's here for example.
+            PlayerId = (int)data[nameof(PlayerId)];
+            PlayerName = (string)data[nameof(PlayerName)];
+            Experience = (float)data[nameof(Experience)];
         }
     }
 }
